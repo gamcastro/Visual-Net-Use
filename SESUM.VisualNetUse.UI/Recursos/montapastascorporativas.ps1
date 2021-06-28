a4b1a827372eb22ee4ebf94929b5d660bd4a55f2
George Castro <gamcastro14@gmail.com>
2021-06-28 14:43:53 -0300

    Refatorado o arquivo de script powershell.

--- SESUM.VisualNetUse.UI/Recursos/montapastascorporativas.ps1
+++ SESUM.VisualNetUse.UI/Recursos/montapastascorporativas.ps1
@@ -1,4 +1,6 @@
-$cred = Get-Credential -Message "Informe seu usuário e senha para acessar suas pastas" 
+
+
+$cred = $host.UI.PromptForCredential("Usuario e senha TRE-MA","Informe seu usuario e senha do TRE (Igual ao do e-mail)" ,"TRE-MA\seu titulo aqui","")
 $letraDoDrive = "Z"
 $caminho = "\\viana-0a\pastas$"
 New-PSDrive -Name $letraDoDrive -Root $caminho -Persist  -Credential $cred -PSProvider FileSystem -Scope Global -ErrorAction Stop | Out-Null
